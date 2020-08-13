c39697a3248bdfd38d06e79907f82eafbefd361f
Ahmadsamak81 <aa.samak81@gmail.com>
2020-08-13 16:48:15 +0300

    Basic First shell

--- first.sh
+++ first.sh
@@ -3,3 +3,5 @@ echo Ahmad # This is my name
 echo Devope #This is Course
 echo "hello # world"
 echo "update2"
+echo $((2#111)) #convert (111) binary to decimal = 7
+echo $((16#AB)) #convert (00AB) Hex to decimal = 171
