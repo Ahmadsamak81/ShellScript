9462cf9fb4c4b4b9c2a03f871db5d66b129088d1
Ahmadsamak81 <aa.samak81@gmail.com>
2020-08-14 14:43:21 +0300

    learning shell scripting if statement

--- first.sh
+++ first.sh
@@ -7,3 +7,14 @@ echo "update2"
 echo "the word $name contains ${#name} charaters"
 echo $((2#111)) #convert (111) binary to decimal = 7
 echo $((16#AB)) #convert (00AB) Hex to decimal = 171
+
+#/////////////////////////////////////////////////////////////////
+#if statement
+
+var=-10
+if [ "$var" -gt 0 ] 
+then
+echo "yes"
+else
+echo "no"	
+fi
