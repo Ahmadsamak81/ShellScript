4b3f87767ee1d40c1bea7c9c846f4d7266325546
Ahmadsamak81 <aa.samak81@gmail.com>
2020-08-14 10:34:26 +0300

    learning shell scripting

--- first.sh
+++ first.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+name=tea
 echo Ahmad # This is my name
 echo Devope #This is Course
 echo "hello # world"
 echo "update2"
+echo "the word $name contains ${#name} charaters"
 echo $((2#111)) #convert (111) binary to decimal = 7
 echo $((16#AB)) #convert (00AB) Hex to decimal = 171
