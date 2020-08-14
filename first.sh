9369eea79b482b4529572702486a4df6e08a14a4
Ahmadsamak81 <aa.samak81@gmail.com>
2020-08-14 16:08:46 +0300

    learning shell scripting if statement

--- first.sh
+++ first.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 name=tea
+
+let "y=((x=20,10/2))"
+echo $y
+
+varr=DSlCoNecTion
+echo ${varr,,}
+
+echo "\" Ahmad Elsamak \""
+
+ls --help  | grep "\-U"
+
+let val=500/2
+echo $val
+val2=`echo $val` 
+echo "$val2"
+
 echo Ahmad # This is my name
 echo Devope #This is Course
 echo "hello # world"
@@ -18,3 +34,24 @@ echo "yes"
 else
 echo "no"	
 fi
+#///////////////////////////////////////////////////////////////
+# for loop
+colors="red black white"
+for col in $colors 
+do
+	echo $col
+done	
+
+#***********************
+
+colors="red black white"
+for col in "$colors"  
+do
+        echo $col
+done
+#***********************
+colors="red black white"
+for col in 'colors'
+do
+        echo $col
+done
