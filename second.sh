4805448002c349b918ee593fa83a3253012a41c2
Ahmadsamak81 <aa.samak81@gmail.com>
2020-08-15 13:45:16 +0300

    learning sell scripting cont. if..then and special way to create file

--- second.sh
+++ second.sh
@@ -7,4 +7,23 @@ then :
 else
 echo $var
 fi
-
+#/////////////////////////////////////////////////
+touch wood.txt
+ ls
+ : > wood.txt # can creat new file with empty using : > xxxx.xxx
+ ls
+ cat wood.txt
+ echo "sdsddsdd" > wood.txt
+ cat wood.txt
+ : > wood.txt
+ cat wood.txt
+ echo "sddsddddd" >wood.txt
+ echo "eeeeeeeee" >> wood.txt
+ cat wood.txt
+ : >> wood.txt
+ cat wood.txt
+ : > wood.txt
+ cat wood.txt
+ rm wood.txt
+ : > wood.txt
+				   
