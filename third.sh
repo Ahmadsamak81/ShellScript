05d0c1b1adeea8e65ae765c55a196f4bbae010e6
Ahmadsamak81 <aa.samak81@gmail.com>
2020-08-15 17:37:48 +0300

    learning sell scripting ...for..loop,if

--- third.sh
+++ third.sh
@@ -12,4 +12,17 @@ for i in 1 2 3 4
 do
 	echo "the value i is $i"
 done
-
+#/////////////////////////////////
+var3=/home/ahmad/ShellScript/wood.txt
+if [[ -e $var3 ]]   #-e  exist -r -x .... etc
+then 
+	echo "file exisits"
+else
+	echo "file does not exist"
+fi	
+#////////////////////////////////////
+echo "I was called with $# parameters"
+echo "My name is $0"
+echo "My first parameter is $1"
+echo "My second parameter is $2"
+echo "All parameters are $@"
