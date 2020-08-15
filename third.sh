3a3d0157aa1242674daaca00d11fe66414c31160
Ahmadsamak81 <aa.samak81@gmail.com>
2020-08-15 15:54:37 +0300

    learning sell scripting ...for..loop,if

--- third.sh
+++ third.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+echo ~+  # pwd
 var=10
 unset var # unset value in variable
 echo $var # nothing print
+#//////////////////////////////////
+echo " type any some value"
+read var2
+echo $var2
+#/////////////////////////////////
+for i in 1 2 3 4
+do
+	echo "the value i is $i"
+done
+
