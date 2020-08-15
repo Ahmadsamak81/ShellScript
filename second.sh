00ea85551e7a65ebd38756757623753ddf6bdf55
Ahmadsamak81 <aa.samak81@gmail.com>
2020-08-15 14:12:03 +0300

    learning sell scripting cont. if..then and special way to create file

--- second.sh
+++ second.sh
@@ -15,8 +15,18 @@ then
 else
 	echo "YES"
 fi	
+#////////////the sam method///////////////
+var1=100
+echo $(( var2 = var1<20?1:0 )) # one line do it 
 
-#/////////////////////// extra //////////////////////////
+if [ "$var1" -lt 20 ]
+then 
+	var2=1
+else
+	var2=0
+fi	
+echo $var2
+#////////////////////// extra //////////////////////////
 touch wood.txt
  ls
  : > wood.txt # can creat new file with empty using : > xxxx.xxx
